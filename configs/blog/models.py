1602fdeb823a8f1e6bf5a4a48b1e7ce59ae5b0db
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-03-08 14:25:12 +0500

    feat: implement models, register admin and fix ALLOWED_HOSTS setings

--- configs/blog/models.py
+++ configs/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Name of Category')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Name of Product')
+    content = models.TextField(verbose_name='Description')
+    photo = models.ImageField(upload_to='photos/', null=True, blank=True, verbose_name='Images')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated date')
+    publish = models.CharField(default=True, verbose_name='Publish')
+    views = models.IntegerField(default=0, verbose_name='Quantity views')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category')
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
--- configs/configs/settings.py
+++ configs/configs/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['#']
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -35,7 +35,9 @@ ROOT_URLCONF = 'configs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/ 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -49,10 +51,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'configs.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -60,9 +58,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -79,20 +74,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'asia-Tashkent'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
